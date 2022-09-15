f458bae2600d1c00285dd36a5c230897d2795841
MijstenHove <mijs.tenhove@hotmail.com>
2022-09-15 17:40:09 +0200

    dev spaans fix

--- dev_spaans_2022/dev_spaans_2022/dev_spaans_2022.cpp
+++ dev_spaans_2022/dev_spaans_2022/dev_spaans_2022.cpp
@@ -5,12 +5,56 @@ using namespace std;
 
 int main()
 {
-	ofstream outFile;
+	std::ifstream dataFile("MijsFile.txt");
+	if (!dataFile.is_open()) {
+		std::cout << "There was an error opening the data file" << std::endl;
+		return 1;
+	}
+	// class data reading
+    std:string className = "";
+	float classNumber = 0.0;
+	dataFile >> className >> classNumber;
 
-	outFile.open("mijsFile.txt");
 
-	int age[]{ 11,12,12,13};
-	string name[]{ "aya", "ben", "cloe", "derk" };
+	//Info that is needed
+	int students = 0;
+	float averageHeight = 0;
+	std::string thirdStudent = "";
+
+	while (!dataFile.eof()) {
+		//Add a student
+		students++;
+		//Store memory to read the data
+		std::string name;
+		float age;
+		float height;
+		//Read the data
+		dataFile >> name >> age >> height;
+		averageHeight += height;
+		//Special case
+		if (students == 3)
+			thirdStudent = name;
+	}
+
+	//Close the file since we're not going to use it anymore
+	dataFile.close();
+
+	//Calculate the average height
+	averageHeight /= (float)students;
+
+	//Print the final result
+	std::cout << "Amount of students: " << students << std::endl;
+	std::cout << "Third student: " << thirdStudent << std::endl;
+	std::cout << "Average height: " << averageHeight;
+
+	std::cout << std::endl << "Los delegados de la clase " << className << " numero " << classNumber << " son dos" << std::endl;
+
+	return 0;
+}
+
+	
+	/*int age[]{ 11,12,12,13};
+	std::string name[]{ "aya", "ben", "cloe", "derk" };
 	float hight[]{ 5.2f,5.1f,5.1f,5.3f };
 
 
@@ -23,6 +67,9 @@ int main()
 
 	int newsize = sizeof(name) / sizeof(*name);
 	cout << "kid amound "<< newsize << endl;
+	// thirsd student 
+	// average hight
+	//
 	outFile.close();
 
 	return 1;
@@ -61,4 +108,3 @@ int main()
 		"class delegate [name] number [class num] is two"*/
 
 
-}
