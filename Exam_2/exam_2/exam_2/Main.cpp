339656faee22591b080841f5b23bb37ddc4ac652
MijstenHove <mijs.tenhove@hotmail.com>
2022-09-25 14:31:46 +0200

    save binery

--- Exam_2/exam_2/exam_2/Main.cpp
+++ Exam_2/exam_2/exam_2/Main.cpp
@@ -1,15 +1,14 @@
-#include <fstream>
-#include <ios>
-#include <istream>
-
+#include<fstream>
+#include <sstream>
+#include <iostream>
 #include <vector>
+
 #include <list>
 using namespace std;
 
 
-	return 0;
-	struct GameObject
-	{
+	
+	struct GameObject{
 		char type;
 		int x; 
 		int y;
@@ -18,15 +17,13 @@ using namespace std;
 	std::vector<GameObject> objecs;
 
 
-	struct Weapon
-	{
+	struct Weapon{
 		char name[10];
 		int power;
 	};
 
 
-	struct Enemy
-	{
+	struct Enemy{
 		char id;
 		int c;
 		float y;
@@ -35,6 +32,43 @@ using namespace std;
 	std::list<Enemy> enemies;
 
 
+	int main() {
+//writing the file ard coded
+		Weapon weapon = { "big ax", 50 };
+
+		fstream f;
+		f.open("save.dat", ios::out | ios::binary);
+
+		if (f.is_open())
+		{
+			f.write(reinterpret_cast<char*>(&weapon), sizeof(Weapon));
+			f.close();
+		}
+		else
+		{
+			std::cout << "error: \n";
+		}
+	
+		//reading the file
+		Weapon newWeapon;
+		f.open("save.dat", ios::out | ios::binary);
+
+		if (f.is_open())
+		{
+			f.read(reinterpret_cast<char*>(&newWeapon), sizeof(Weapon));
+			f.close();
+		}
+		else
+		{
+			std::cout << "error: \n";
+		}
+
+		cout << weapon.name << endl;
+		cout << newWeapon.power << endl;
+
+
+
+	}
 
 
 void SaveObjects(std::vector<GameObject> o, std::string fileName) {
